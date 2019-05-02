d5cd2155ff07c032b4ce100dae480c4f9da6b9e9
efrain <efarinva98@gmail.com>
2019-05-02 18:46:40 -0400

    picture

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// cam also use dot notation like 
+import Hog from './Hogwarts_houses.jpg';
+import crow from './crow.jpg';
+import lion from './lion.jpg';
+import skunk from './skunk.jpg'; 
+import snake from './snake.jpg';
 import './App.css';
+import Button from './components/Button';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <img id = 'pic' src={Hog}/>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          
+          <h1 className="App-title">Welcome to Hogwarts</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, press sort
         </p>
+        <div>
+          <div>
+          <Button/>
+            <img src={crow}/>
+            <img src={lion}/>
+            <img src={snake}/>
+            <img src={skunk}/>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ react-sorting-hat/src/components/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import './button.css' ;
+
+const Button = () => {
+  return (
+    <div>
+      
+    {/* <input/> */}
+    <button>sort</button>
+    </div>
+  )
+}
+export default Button;
